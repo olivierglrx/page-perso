4cce7803e8ff21e4a1f92f6fee54b1f35a33bc88
olivierglrx <olivier.glrx@gmail.com>
2024-07-04 10:51:41 +0200

    devoirs

--- pages/devoirs.vue
+++ pages/devoirs.vue
@@ -1,8 +1,28 @@
 <template>
   <Titleheader title="Devoirs" />
+  <UButton label="Connectez-vous" @click="isOpen2 = true" />
 
-  <div class="flex h-screen mx-10">
+  <UModal v-model="isOpen2">
+    <div class="p-4">
+      <label> Connectez vous pour accéder aux notes</label>
+      <UInput v-model="pass" />
+      <UButton
+        class="bg-blue-500 hover:bg-blue-600 dark:bg-blue-500"
+        size="sm"
+        @click="validate(pass)"
+        >OK</UButton
+      >
+      <p>{{ message }}</p>
+    </div>
+  </UModal>
+
+  <div class="md:flex flex-wrap h-screen mx-10">
     <ul class="md:w-1/2 w-11/12">
+      <h2
+        class="mt-2 mb-4 text-1xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-2xl dark:text-white"
+      >
+        DS
+      </h2>
       <li v-for="item in DS" class="">
         <div
           class="dark:bg-gray-700 m-3 shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform"
@@ -20,8 +40,22 @@
               </nuxt-link>
               {{ item.date }}
             </div>
-            <div v-if="store.logged">
-              <p v-if="item.toNotes">
+            <div v-if="item.showCorrection">
+              <Icon
+                name="mdi:file-document"
+                color="black dark:white"
+              /><nuxt-link
+                :to="item.to"
+                external
+                class="text-blue-600 font-semibold"
+                >Correction
+              </nuxt-link>
+              {{ item.date }}
+            </div>
+
+            <p v-if="!item.toNotes">Notes à venir</p>
+            <div v-else>
+              <div v-if="store.logged">
                 <Icon
                   name="mdi:file-document"
                   color="black dark:white"
@@ -31,31 +65,70 @@
                   class="text-blue-600 font-semibold"
                   >Notes
                 </nuxt-link>
-              </p>
-              <p v-else>Notes à venir</p>
+              </div>
+              <p v-else>Connectez-vous</p>
             </div>
-            <div v-else>
-              <button
-                label="Open"
-                @click="isOpen = true"
-                class="text-blue-600 font-semibold"
-              >
-                Notes
-              </button>
+          </div>
+          <div>
+            <span v-for="key in item.keywords.slice(0, -1)">
+              {{ key + ", " }}</span
+            >
+            <span> {{ item.keywords.slice(-1)[0] }}.</span>
+          </div>
+        </div>
+      </li>
+    </ul>
 
-              <UModal v-model="isOpen">
-                <div class="p-4">
-                  <label> Connectez vous pour accéder aux notes</label>
-                  <UInput v-model="pass" />
-                  <UButton
-                    class="bg-blue-500 hover:bg-blue-600 dark:bg-blue-500"
-                    size="sm"
-                    @click="validate(pass)"
-                    >OK</UButton
-                  >
-                  <p>{{ message }}</p>
-                </div>
-              </UModal>
+    <ul class="md:w-1/2 w-11/12">
+      <h2
+        class="mt-2 mb-4 text-1xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-2xl dark:text-white"
+      >
+        Interro
+      </h2>
+      <li v-for="item in Interro" class="">
+        <div
+          class="dark:bg-gray-700 m-3 shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform"
+        >
+          <div class="flex justify-between">
+            <div>
+              <Icon
+                name="mdi:file-document"
+                color="black dark:white"
+              /><nuxt-link
+                :to="item.to"
+                external
+                class="text-blue-600 font-semibold"
+                >{{ item.name }}
+              </nuxt-link>
+              {{ item.date }}
+            </div>
+            <div v-if="item.showCorrection">
+              <Icon
+                name="mdi:file-document"
+                color="black dark:white"
+              /><nuxt-link
+                :to="item.to"
+                external
+                class="text-blue-600 font-semibold"
+                >Correction
+              </nuxt-link>
+              {{ item.date }}
+            </div>
+
+            <p v-if="!item.toNotes">Notes à venir</p>
+            <div v-else>
+              <div v-if="store.logged">
+                <Icon
+                  name="mdi:file-document"
+                  color="black dark:white"
+                /><nuxt-link
+                  :to="item.toNotes"
+                  external
+                  class="text-blue-600 font-semibold"
+                  >Notes
+                </nuxt-link>
+              </div>
+              <p v-else>Connectez-vous</p>
             </div>
           </div>
           <div>
@@ -74,17 +147,17 @@
 const DS = [
   {
     name: "DS2",
-    to: "/2023/DS2.pdf",
-    toNotes: "",
-    showCorrection: true,
+    to: "DS2.pdf",
+    toNotes: "notesDs2.pdf",
+    showCorrection: "correction.pdf",
     date: "",
     keywords: ["sommes"],
   },
   {
     name: "DS1",
-    to: "2023/DS1.pdf",
+    to: "DS1.pdf",
     toNotes: "",
-    showCorrection: true,
+    showCorrection: "",
     keywords: ["equations", "fonctions"],
   },
 ];
@@ -92,14 +165,14 @@ const DS = [
 const Interro = [
   {
     name: "Interro2",
-    to: "devoirs/2023/DS2.pdf",
+    to: "devoirs/DS2.pdf",
     toNotes: "",
     showCorrection: true,
     keywords: [""],
   },
   {
     name: "Interro1",
-    to: "devoirs2023/DS1.pdf",
+    to: "devoirs/DS1.pdf",
     toNotes: "",
     showCorrection: true,
     keywords: [""],
@@ -108,13 +181,14 @@ const Interro = [
 
 import { useLogginStore } from "@/stores/loggin";
 const isOpen = ref(false);
+const isOpen2 = ref(false);
 const pass = ref("");
 const store = useLogginStore();
 const message = ref("");
 
 function validate(pass) {
   if (pass == "Chaptal2024") {
-    isOpen.value = !isOpen.value;
+    isOpen2.value = !isOpen2.value;
     store.logged = true;
   } else {
     message.value = "Mauvais mot de passe";
