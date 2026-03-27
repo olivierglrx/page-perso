652236a1adff95e496f1dfd4f28066933a162a55
olivierglrx <olivier.glrx@gmail.com>
2026-03-27 09:07:35 +0100

    hero

--- components/hero.vue
+++ components/hero.vue
@@ -1,36 +1,46 @@
 <template>
   <div id="container" class="mt-1 flex">
     <div class="flex">
-      <p class="hidden md:block p-2 mb-10">
+      <div class="hidden md:block p-2 mb-10">
         <ul>
-          <li class="p-2"> Bienvenue sur le site personnel d'Olivier Glorieux   <UAvatar
-            src="olivier.png"
-            alt="Avatar"
-          />.  </li>
-          <li> <Icon name="ph:student-fill" color="black dark:white" /> Etudiants,  vous trouverez ici les documents de cours de la BCPST1 
-            du lycée Chaptal.  </li>
-            <li> <Icon name="uil:github" color="black dark:white"  /> Enseignants, vous avez accés aux fichiers .tex de ces documents sur 
-              <nuxtLink class="text-blue-700 dark:text-blue-300"  to="https://github.com/olivierglrx/BCPST
-              ">https://github.com/olivierglrx/BCPST.
-            </nuxtLink></li>
-            <li> <Icon name="simple-icons:arxiv" color="black dark:white" /> Chercheurs, vous pouvez jeter un oeil à mes anciennes  
-              <nuxt-link class="text-blue-700 dark:text-blue-300" to="publications">publications.</nuxt-link></li>
-            
+          <li class="p-2">
+            Bienvenue sur le site personnel d'Olivier Glorieux
+            <UAvatar src="olivier.png" alt="Avatar" />.
+          </li>
+          <li>
+            <Icon name="ph:student-fill" color="black dark:white" /> Etudiants,
+            vous trouverez ici les documents de cours de la BCPST1 du lycée
+            Chaptal.
+          </li>
+          <li>
+            <Icon name="uil:github" color="black dark:white" /> Enseignants,
+            vous avez accés aux fichiers .tex de ces documents sur
+            <nuxtLink
+              class="text-blue-700 dark:text-blue-300"
+              to="https://github.com/olivierglrx/BCPST
+              "
+              >https://github.com/olivierglrx/BCPST.
+            </nuxtLink>
+          </li>
+          <li>
+            <Icon name="simple-icons:arxiv" color="black dark:white" />
+            Chercheurs, vous pouvez jeter un oeil à mes anciennes
+            <nuxt-link
+              class="text-blue-700 dark:text-blue-300"
+              to="publications"
+              >publications.</nuxt-link
+            >
+          </li>
         </ul>
-
-      </p>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 #container {
-  
   margin-bottom: 1%;
   margin-left: 20%;
- 
-
-  
 }
 
 img {
@@ -39,6 +49,5 @@ img {
 }
 
 p {
-  
 }
 </style>
